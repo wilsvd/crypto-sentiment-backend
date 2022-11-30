130ef8b0a7ca25de145a1490e8bd13b050742ee0
wil <psyws2@nottingham.ac.uk>
2022-11-30 10:58:27 +0000

    feat: add a login page

--- Frontend/learn/firebaseConfig.ts
+++ Frontend/learn/firebaseConfig.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database";
 import { getAuth } from "firebase/auth";
+import { Console } from "console";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -24,3 +25,5 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
 export const auth = getAuth(app);
+
+console.log(auth);

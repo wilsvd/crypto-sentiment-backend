cc71388561faa370dd3b75f7283e34388350e313
wil <psyws2@nottingham.ac.uk>
2022-12-01 22:06:06 +0000

    fix: change path to work with cron

--- Backend/scheduler.py
+++ Backend/scheduler.py
@@ -1,12 +1,19 @@
 from data_collector import DataCollector
 import json
 
+import os
+dir_path = os.path.dirname(os.path.abspath(__file__))
+
+print(dir_path)
+sentiment_path = os.path.join(dir_path, "sentiment/sentiment_data.json")
+print(sentiment_path)
+
 data = DataCollector()
 
 def runner():
     print("JOB START")
     overall_sentiment = data.find_coin_sentiments()
-    with open("./sentiment/sentiment_data1.json", "w") as outfile:
+    with open(sentiment_path, "w") as outfile:
                 json.dump(overall_sentiment, outfile)
     print("JOB DONE")
 

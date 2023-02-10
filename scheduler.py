aa686cba7847789612c693a277b78210e5ad66cd
wil <psyws2@nottingham.ac.uk>
2023-02-10 20:31:22 +0000

    fix: enable correct setup of OAuth for 600 requests

--- scheduler.py
+++ scheduler.py
@@ -2,8 +2,10 @@ from sentiment_collector import SentimentCollector
 from coin_collectors import CoinCollector
 
 import json
+import time
 
 import os
+
 dir_path = os.path.dirname(os.path.abspath(__file__))
 
 print(dir_path)
@@ -13,18 +15,119 @@ print(sentiment_path)
 data = SentimentCollector()
 coins = CoinCollector()
 
-subreddits = coins._get_coin_subreddits()
+# subreddits = coins._get_coin_subreddits()
+# print(subreddits)
 
-import time
+SUBREDDITS = [
+    "bitcoin",
+    "ethereum",
+    "bnbchainofficial",
+    "ripple",
+    "cardano",
+    "0xPolygon",
+    "dogecoin",
+    "solana",
+    "dot",
+    "SHIBArmy",
+    "litecoin",
+    "Tronix",
+    "Avax",
+    "Uniswap",
+    "cosmosnetwork",
+    "chainlink",
+    "bitfinex",
+    "EthereumClassic",
+    "monero",
+    "Bitcoincash",
+    "okx",
+    "stellar",
+    "hedera",
+    "lidofinance",
+    "Crypto_com",
+    "AlgorandOfficial",
+    "vechain",
+    "QuantNetwork",
+    "dfinity",
+    "thegraph",
+    "decentraland",
+    "FantomFoundation",
+    "EOS",
+    "Aave_Official",
+    "theta_network",
+    "AxieInfinity",
+    "tezos",
+    "terraluna",
+    "HuobiGlobal",
+    "kucoin",
+    "ImmutableX/",
+    "zec",
+    "pancakeswap/",
+    "MakerDAO",
+    "CurveDAO",
+    "MinaProtocol",
+    "BittorrentToken",
+    "ecash",
+    "dashpay",
+    "Iota",
+    "NEO",
+    "synthetix_io",
+    "Gemini",
+    "klaytn",
+    "trustwallet",
+    "thorchain",
+    "rocketpool",
+    "SingularityNet",
+    "loopringorg",
+    "EnjinCoin",
+    "zilliqa",
+    "1inch",
+    "nexo",
+    "CasperCSPR",
+    "BATProject",
+    "dydxprotocol",
+    "stacks",
+    "terraluna",
+    "xinfin",
+    "WOO_X",
+    "holochain",
+    "decred",
+    "Ravencoin",
+    "CeloHQ",
+    "oasisnetwork/",
+    "Arweave",
+    "FetchAI_Community",
+    "kava_platform/",
+    "HeliumNetwork",
+    "thresholdnetwork",
+    "theta_network",
+    "harmony_one/",
+    "GoGalaGames/",
+    "Qtum",
+    "BitcoinGoldHQ",
+    "gnosisPM",
+    "Ankrofficial/",
+    "SushiSwap",
+    "SSVnetwork/",
+    "IoTex",
+    "oceanprotocol",
+    "Wavesplatform",
+    "chia",
+    "StepN",
+    "moonbeam",
+    "SHIBArmy",
+    "GolemProject",
+    "livepeer",
+    "MaskNetwork",
+    "kadena/",
+]
+
 
 def runner():
-    start = time.time()
     print("JOB START")
-    overall_sentiment = data.find_crypto_sentiments(subreddits)
-    # with open(sentiment_path, "w") as outfile:
-    #             json.dump(overall_sentiment, outfile)
-    print("JOB DONE")
+    start = time.time()
+    data.find_crypto_sentiments(SUBREDDITS)
     end = time.time()
+    print("JOB DONE")
     print(f"Time taken: {end-start}")
 
 
--- sentiment_collector.py
+++ sentiment_collector.py
@@ -1,8 +1,8 @@
 from reddit_api import RedditAPI
-from process_text import TextProcessor
-from sentiment_classifier import SentimentClassifier
 
-import numpy as np
+import time
+
+import praw.reddit
 
 # from threading import Thread
 from concurrent.futures import ThreadPoolExecutor
@@ -11,77 +11,69 @@ from concurrent.futures import ThreadPoolExecutor
 # TODO: Use the post limit specified
 POST_LIMIT = 50
 
-class SentimentCollector():
-        
+
+class SentimentCollector:
+
     m_ids = []
 
-    def __init__(self) -> None:    
+    def __init__(self) -> None:
         self.r_api = RedditAPI()
-        self.classifier = SentimentClassifier()
 
     # TODO: Optimise _get_submission function.
     def _get_submission(self, reddit_helper, subreddit):
 
-        total_sentiment = 0
-        post_count = 0
-        # NOTE: PRAW is fetching 100 submissions in one request.
-
         try:
-            submissions = reddit_helper.subreddit(subreddit).hot()
+
+            submissions = reddit_helper.subreddit(subreddit).hot(limit=POST_LIMIT)
+            # PRAW Uses lazy loading so the fetch only occurs once the data is actually going to be used.
+            count = 0
             for submission in submissions:
-                if (submission.stickied == False and TextProcessor().is_question(submission.title)):
-                    sentiment = self.classifier.predict_sentiment(submission.title)
-                    post_count += 1
-                    total_sentiment += sentiment
-            # Account for a division by 0 error by just returning None (Effectively ignoring the subreddit)
-            if post_count == 0:
-                return None
-            
-            result = {"sentiment": round(total_sentiment/post_count, 2)}              
-            return result
+                count += 1
         except:
             print("Subreddit does not exist")
-            return None        
-      
-    # def _get_valid_subreddit(self, subreddits_info):
-    #         options = self.reddit.subreddits.search_by_name(subreddits_info) # Searches for the subreddit.
-    #         if len(options) > 0:
-    #             return str(options[0])
 
     def find_crypto_sentiments(self, total_subreddits):
-        # overall_feeling = {}
 
-        # Partition total_subreddits as evenly as possible
-        partitions = np.array_split(total_subreddits, 4)
+        # NOTE: Partition when using Alternating/Multi-threading
+        # partitions = np.array_split(total_subreddits, 4)
 
-        with ThreadPoolExecutor(max_workers=4) as executor:
-            for worker, partition in enumerate(partitions):
-                reddit_helper = self.r_api.connect_to_reddit(worker)
-                executor.submit(self.find_partition_sentiment, partition, reddit_helper)
+        # NOTE: Single Acc/Single PRAW
+        # single = self.r_api.single_instance()
+        # reddit_helper = single[0]
+        # for subreddit in total_subreddits:
+        # self._get_submission(reddit_helper, subreddit)
 
-        # for worker, partition in enumerate(partitions):
-        #     reddit_helper = self.r_api.connect_to_reddit(worker)
-        #     t = Thread(target=self.find_partition_sentiment, args=(partition, reddit_helper))
-        #     t.start()
+        # single_multi = self.r_api.single_acc_multi_instance()
+        # multi_multi = self.r_api.multi_acc_multi_instance()
 
+        # print(multi_multi[0].auth.limits)
+        # print(multi_multi[1].auth.limits)
+        # print(multi_multi[2].auth.limits)
+
+        # NOTE: Single Acc/Multi PRAW --- Alternating
         # for worker, subreddit in enumerate(total_subreddits):
         #     worker_index = worker % 4
-        #     reddit_helper = self.worker_queue[worker_index]
-        #     result = self._get_submission(reddit_helper, subreddit)
-        #     if result:
-        #         overall_feeling[subreddit] = result        
-        
+        #     reddit_helper = single_multi[worker_index]
+        #     print(reddit_helper.auth.limits)
+        #     self._get_submission(reddit_helper, subreddit)
+
+        # NOTE: Multi Acc/Multi PRAW --- Alternating
+        # for worker, subreddit in enumerate(total_subreddits):
+        #     worker_index = worker % 4
+        #     reddit_helper = self.multi_multi[worker_index]
+        #     self._get_submission(reddit_helper, subreddit)
+
+        # NOTE: Single Acc/Multi PRAW --- Multi-threading
+
+        # NOTE: Multi Acc/Multi PRAW --- Multi-threading
+
         # return overall_feeling
 
-    def find_partition_sentiment(self, partition, reddit_helper):
-        partition_sentiment = {}
-        for subreddit in partition:
-            result = self._get_submission(reddit_helper, subreddit)
-            if result:
-                partition_sentiment[subreddit] = result  
-        print(partition_sentiment)
-        # return partition_sentiment
-
-# Top 10
-
-
+    # def find_partition_sentiment(self, partition, reddit_helper):
+    #     partition_sentiment = {}
+    #     for subreddit in partition:
+    #         result = self._get_submission(reddit_helper, subreddit)
+    #         if result:
+    #             partition_sentiment[subreddit] = result
+    #     print(partition_sentiment)
+    # return partition_sentiment

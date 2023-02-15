7b2466b55c3751d95f11ee1bdaddbb00d5955844
wil <psyws2@nottingham.ac.uk>
2023-02-15 18:31:53 +0000

    refactor: use name-main and remove print statements

--- scheduler.py
+++ scheduler.py
@@ -1,32 +1,15 @@
-from sentiment_collector import SentimentCollector
-from coin_collectors import CoinCollector
+from post_collector import SentimentCollector
+from coin_collector import CoinCollector
 from firebase import FirebaseDatabase
 
-import time
-import json
 import os
 
-dir_path = os.path.dirname(os.path.abspath(__file__))
-
-print(dir_path)
-sentiment_path = os.path.join(dir_path, "sentiment/sentiment_data.json")
-print(sentiment_path)
-
-data = SentimentCollector()
-coins = CoinCollector()
-database = FirebaseDatabase()
-
-subreddits = coins.get_coin_subreddits()
-
-
-def runner():
-    start = time.time()
-    print("JOB START")
+if __name__ == "__main__":
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    sentiment_path = os.path.join(dir_path, "sentiment/sentiment_data.json")
+    data = SentimentCollector()
+    coins = CoinCollector()
+    database = FirebaseDatabase()
+    subreddits = coins.get_coin_subreddits()
     overall_sentiment = data.find_crypto_sentiments(subreddits)
     database.set_data(overall_sentiment)
-    print("JOB DONE")
-    end = time.time()
-    print(f"Time taken: {end-start}")
-
-
-runner()
